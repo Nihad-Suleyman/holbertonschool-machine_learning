21e2ccf2ea9b413ee2611ecdf8940be8195bebae
Nihad <nihad>
2026-01-10 09:22:31 +0000

    done

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
+import pandas as pd
 """let's start with creating function"""
 def from_numpy(array):
-    import pandas as pd
+    """let's assign our df"""
     df = pd.DataFrame(array)
     df.columns = [chr(ord('A') + i) for i in range(array.shape[1])]
     return df
