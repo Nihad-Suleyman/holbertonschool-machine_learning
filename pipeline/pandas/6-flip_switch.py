1e8f967b962aa15b7380458ad6559204bc0eba57
Nihad <Nihad>
2026-01-10 13:13:32 +0000

    done

---
+++ pipeline/pandas/6-flip_switch.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""now we will flip the related data and sort it in ascendinng order"""
+
+
+def flip_switch(df):
+    """Docstring for flip_switch"""
+    df.sort_index(ascending=False)
+    df.columns, df.rows = df.rows, df.columns
+    return df
