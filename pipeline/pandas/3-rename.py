706dee68489dfe618471128903af1af68df24fc2
Nihad <nihad>
2026-01-10 10:12:47 +0000

    done

---
+++ pipeline/pandas/3-rename.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""now let's do with using filename"""
+
+
+import pandas as pd
+
+
+def rename(df):
+    """create a DataFrame from a file"""
+    df = df.rename(columns={'TimeStamp': 'DateTime'})
+    df['DateTime'] = pd.to_datetime(df['DateTime'], unit='s')
+    df = df[['DateTime', 'Close']]
+
+    return df
