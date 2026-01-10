f563ece4e4b00240482aa65a4bef9c84bfcab3c3
Nihad <Nihad>
2026-01-10 12:58:08 +0000

    done

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -4,6 +4,4 @@
 
 def slice(df):
     """we will now look at one type of slicing in pandas"""
-    df.columns = ['High', 'Low', 'Close', 'Volume_BTC']
-    arr = df.iloc[::60]
-    return arr
+    return df[['High', 'Low', 'Close', 'Volume_BTC']].iloc[::60]
