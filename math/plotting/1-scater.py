2794469eebdda0a818f410a803e0ffe6232f9ac2
Nihad <Nihad>
2026-01-10 17:08:05 +0000

    done

---
+++ math/plotting/1-scater.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Now we will evaluate scatter"""
+
+
+def scatter():
+    """we will have normal distribution kkind of plot"""
+    mean = [69, 9]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+    plt.scatter(x, y, c='magenta')
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
+    plt.title("Men's Height vs Weight")
