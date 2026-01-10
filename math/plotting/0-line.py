ee5d72cd20b7812bbf363f59f8d094c96f990f0a
Nihad <Nihad>
2026-01-10 16:54:01 +0000

    done

---
+++ math/plotting/0-line.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""now we will delve into plotting"""
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def line():
+    """we will evaluate a cubic function"""
+    y = np.arangre(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(y, color='red')
