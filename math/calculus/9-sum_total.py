f3cc06384fe5b735c3540ce52f37b4beed7984e8
Nihad <NIhad>
2026-01-16 13:56:50 +0000

    done

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,7 +6,6 @@ def summation_i_squared(n):
     """we will sum the square of numbers"""
     if !isnum(n):
         return None
-    cnt = 0
-    for i in range(n):
-        cnt += i**2
-    return cnt
+    if n == 1:
+        return 1
+    return n*(n + 1)/2 + n + summation_i_squared(n-1)
