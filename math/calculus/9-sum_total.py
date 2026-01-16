760a536d615ad09a5a0c43651dc5c873a1aec1ee
Nihad <NIhad>
2026-01-16 14:00:17 +0000

    done

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,7 +4,7 @@
 
 def summation_i_squared(n):
     """we will sum the square of numbers"""
-    if isinstance(n) == False:
+    if isinstance(n, int) == False:
         return None
     if n == 1:
         return 1
