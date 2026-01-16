0dfa635f6eb45e25f815874e2b5c641795a839c7
Nihad <NIhad>
2026-01-16 14:55:17 +0000

    done

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
-"""Now we will look at an integral problem"""
+"""Polynomial integration."""
 
 
 def poly_integral(poly, C=0):
-    """We will return the list of coefficients of an integral"""
-    if (not isinstance(poly, list)) or (not isinstance(C, int)):
+    """Return the integral of a polynomial represented by a list of coefficients."""
+    if not isinstance(poly, list) or len(poly) == 0 or not isinstance(C, int):
         return None
-    new_poly = [C]
-    for i in range(1, len(poly)):
-        new_poly.append(poly[i - 1] // (i))
-    return new_poly
+    for coeff in poly:
+        if not isinstance(coeff, (int, float)):
+            return None
+    integ = [C]
+    for i, coeff in enumerate(poly):
+        val = coeff / (i + 1)
+        if val.is_integer():
+            val = int(val)
+        integ.append(val)
+    while len(integ) > 1 and integ[-1] == 0:
+        integ.pop()
+    return integ
