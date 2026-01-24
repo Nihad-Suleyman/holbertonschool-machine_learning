c40854a9f27550e422de02ec7518479f9bd9a62f
Nihad <nihad>
2026-01-24 23:23:48 +0400

    doone

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def matrix_shape(matrix):
+    if not isinstance(matrix, list):
+        return []
+    if isinstance(matrix[0], list):
+        return [len(matrix)] + matrix_shape(matrix[0])
+    return [len(matrix)]
