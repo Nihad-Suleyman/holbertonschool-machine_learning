cc3c4db9cf07ecd25080a19f3b8a5de15a45eea0
Nihad <nihad>
2026-01-24 23:25:06 +0400

    now done

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
+    """Okay, so we will look at how to go deep down of matrix size"""
     if not isinstance(matrix, list):
         return []
     if isinstance(matrix[0], list):
