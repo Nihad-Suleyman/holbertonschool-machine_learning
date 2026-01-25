0c5f7cfcd44dbcd16ad3a6a2328714eff100b489
Nihad <nihad>
2026-01-25 23:14:30 +0400

    now okay finally last

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""we will look at matrix multiplication this time at numpy"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """we will just use numpy method"""
+    return mat1 @ mat2
