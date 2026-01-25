c310573cbdd6b9f57adc122a6160b6ac4587ec37
Nihad <nihad>
2026-01-25 17:15:07 +0400

    okay

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -7,9 +7,9 @@ def add_matrices2D(mat1, mat2):
     if len(mat1) != len(mat2):
         return None
     new = []
-    for j in range(len(mat1[0])):
+    for j in range(len(mat1)):
         rows = []
-        for i in range(len(mat1)):
+        for i in range(len(mat1[0])):
             rows.append(mat1[j][i] + mat2[j][i])
         new.append(rows)
     return new
