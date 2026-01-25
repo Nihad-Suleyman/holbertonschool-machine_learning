4325aa28fb395848eccfaaab120118b98c535bf2
Nihad <nihad>
2026-01-25 15:44:47 +0400

    okay finally

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -4,4 +4,6 @@
 
 def add_arrays(arr1, arr2):
     """This is our specific funtion, we will just add."""
+    if len(arr1) != len(arr2):
+        return None
     return [arr1[i] + arr2[i] for i in range(min(len(arr1), len(arr2)))]
