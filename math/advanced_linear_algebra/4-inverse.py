e7ba8ed8f747e695b14aaad1857583a0ff4988bf
Nihad <nihad>
2026-02-07 23:42:07 +1000

    done

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -75,6 +75,6 @@ def inverse(matrix):
             return None
         co = cofactor(matrix)
         adj = [[co[i][j] for i in range(len(co))] for j in range(len(co))]
-        inv = [[adj[i][j] / det for j in range(len(adj))] 
+        inv = [[adj[i][j] / det for j in range(len(adj))]
                for i in range(len(adj))]
         return inv
