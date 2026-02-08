91d495c5eeb4905abcc39b35b5a52e31f1314858
Nihad <nihad>
2026-02-08 17:20:50 +0400

    maybe this version works

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -12,12 +12,14 @@ class Binomial:
             if len(data) < 2:
                 raise ValueError('data must contain multiple values')
             mean = sum(i for i in data) / len(data)
-            p = mean / len(data)
-            n = mean / p
+            var = sum((x - mean) ** 2 / len(data) for x in data)
+            p = 1 - var / mean
+            n = round(mean / p)
+            p = mean / n
         else:
-            if n < 0:
+            if n <= 0:
                 raise ValueError('n must be a positive value')
-            if p < 0 or p > 1:
+            if p <= 0 or p >= 1:
                 raise ValueError('p must be greater than 0 and less than 1')
         self.n = round(n)
-        self.p = p
+        self.p = float(p)
