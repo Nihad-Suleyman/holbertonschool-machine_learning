66a5334d6703a557e16d5853daba0e7469ed58bd
Nihad <nihad>
2026-02-08 18:54:27 +0400

    for sure

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -8,7 +8,8 @@ def likelihood(x, n, P):
     if not isinstance(n, int) or n <= 0:
         raise ValueError('n must be a positive integer')
     if not isinstance(x, int) or x < 0:
-        raise ValueError('x must be an integer that is greater than or equal to 0')
+        raise ValueError(
+            'x must be an integer that is greater than or equal to 0')
     if x > n:
         raise ValueError('x cannot be greater than n')
     if not isinstance(P, np.ndarray) or P.ndim != 1:
@@ -22,5 +23,5 @@ def likelihood(x, n, P):
         factk *= j
     for k in range(1, n + 1 - x):
         factnk *= k
-    return np.array([factn / (factk * factnk) * p ** x * ((1 - p) ** (n - x)) 
+    return np.array([factn / (factk * factnk) * p ** x * ((1 - p) ** (n - x))
                      for p in P])
