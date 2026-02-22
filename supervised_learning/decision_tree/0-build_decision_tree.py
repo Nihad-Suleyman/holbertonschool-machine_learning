44a1403b922369e03873d4c1c9291e449be027aa
Nihad <Nihad>
2026-02-22 23:01:43 +0400

    maybe now >?

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -2,6 +2,9 @@
 """maximum depth exercise."""
 
 
+import numpy as np
+
+
 class Node:
     def __init__(self, feature=None, threshold=None,
                  left_child=None, right_child=None,
@@ -16,7 +19,36 @@ class Node:
         self.depth = depth
 
     def max_depth_below(self):
-        """Return maximum depth below this node"""
         left_depth = self.left_child.max_depth_below()
         right_depth = self.right_child.max_depth_below()
         return max(left_depth, right_depth)
+
+
+class Leaf(Node):
+    def __init__(self, value, depth=None):
+        super().__init__()
+        self.value = value
+        self.is_leaf = True
+        self.depth = depth
+
+    def max_depth_below(self):
+        return self.depth
+
+
+class Decision_Tree:
+    def __init__(self, max_depth=10, min_pop=1, seed=0,
+                 split_criterion="random", root=None):
+        self.rng = np.random.default_rng(seed)
+        if root:
+            self.root = root
+        else:
+            self.root = Node(is_root=True)
+        self.explanatory = None
+        self.target = None
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.split_criterion = split_criterion
+        self.predict = None
+
+    def depth(self):
+        return self.root.max_depth_below()
