e0eff4bf3c2acee8aa45e859ace222c3c5620208
Nihad <Nihad>
2026-02-22 17:14:10 +0400

    maybe now

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
-"""Calculates the F1 score for each class"""
+"""Calculates specificity for each class"""
+
 
 import numpy as np
 
 
-def f1_score(confusion):
-    """Calculates the F1 score for each class"""
+def specificity(confusion):
+    """Calculates specificity for each class"""
+    total = np.sum(confusion)
     tp = np.diag(confusion)
-    precision = tp / np.sum(confusion, axis=0)
-    recall = tp / np.sum(confusion, axis=1)
+    row_sum = np.sum(confusion, axis=1)
+    col_sum = np.sum(confusion, axis=0)
 
-    return 2 * (precision * recall) / (precision + recall)
+    fp = col_sum - tp
+    fn = row_sum - tp
+    tn = total - (tp + fp + fn)
+
+    return tn / (tn + fp)
