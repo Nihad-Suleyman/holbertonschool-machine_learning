0d7e23c020377add4c6d7fc5c16a33876b2c56b5
Nihad <Nihad>
2026-02-22 22:20:15 +0400

    task 0 + readme.md

---
+++ decision_tree/0-build_decision_tree.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""maximum depth exercise."""
+
+
+def max_depth_below(self):
+    """We will take leftmost and rightmost"""
+    left_depth = self.left_child.max_depth_below()
+    right_depth = self.right_child.max_depth_below()
+    return max(left_depth, right_depth)
---
+++ decision_tree/README.md
@@ -0,0 +1,1 @@
+In this folder we will look at sample decision tree exercises.
