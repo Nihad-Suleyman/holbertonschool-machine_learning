4fef0caa64e160b9724eceed037ce8d9e2b3b7b9
Nihad <Nihad>
2026-02-22 23:07:05 +0400

    documentation issues

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -10,6 +10,7 @@ class Node:
     def __init__(self, feature=None, threshold=None,
                  left_child=None, right_child=None,
                  is_root=False, depth=0):
+        """initializing"""
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -20,6 +21,7 @@ class Node:
         self.depth = depth
 
     def max_depth_below(self):
+        """max depth function"""
         left_depth = self.left_child.max_depth_below()
         right_depth = self.right_child.max_depth_below()
         return max(left_depth, right_depth)
@@ -28,18 +30,22 @@ class Node:
 class Leaf(Node):
     """child class of Node"""
     def __init__(self, value, depth=None):
+        """intializing"""
         super().__init__()
         self.value = value
         self.is_leaf = True
         self.depth = depth
 
     def max_depth_below(self):
+        """returning depth"""
         return self.depth
 
 
 class Decision_Tree:
+    """decision tree class"""
     def __init__(self, max_depth=10, min_pop=1, seed=0,
                  split_criterion="random", root=None):
+        """initializing"""
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
@@ -53,4 +59,5 @@ class Decision_Tree:
         self.predict = None
 
     def depth(self):
+        """returning depth"""
         return self.root.max_depth_below()
