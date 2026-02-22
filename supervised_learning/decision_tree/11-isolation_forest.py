0d3abee78fc28a142f3555927d643d5a4d072e14
Nihad <Nihad>
2026-02-22 23:34:55 +0400

    task 11 finally

--- supervised_learning/decision_tree/11-isolation_forest.py
+++ supervised_learning/decision_tree/11-isolation_forest.py
@@ -1,133 +1,48 @@
 #!/usr/bin/env python3
-"""Isolation Random Tree"""
+"""Nice Random"""
 import numpy as np
-Node = __import__('8-build_decision_tree').Node
-Leaf = __import__('8-build_decision_tree').Leaf
+Isolation_Random_Tree = __import__('10-isolation_tree').Isolation_Random_Tree
 
 
-class Isolation_Random_Tree:
-    """Isolation Random Tree"""
-
-    def __init__(self, max_depth=10, seed=0, root=None):
-        """Constructor"""
-        self.rng = np.random.default_rng(seed)
+class Isolation_Random_Forest():
+    """ Isolation_Random_Forest"""
+    def __init__(self, n_trees=100, max_depth=10, min_pop=1, seed=0):
+        self.numpy_predicts = []
+        self.target = None
+        self.numpy_preds = None
+        self.n_trees = n_trees
         self.max_depth = max_depth
-        self.predict = None
-        self.min_pop = 1
-        self.explanatory = None
+        self.seed = seed
 
-        if root:
-            self.root = root
-        else:
-            self.root = Node(is_root=True)
+    def predict(self, explanatory):
+        """ predicts the target """
+        predictions = np.array([f(explanatory) for f in self.numpy_preds])
+        return predictions.mean(axis=0)
 
-    def __str__(self):
-        """String representation"""
-        return self.root.__str__() + "\n"
-
-    def depth(self):
-        """Depth of the tree"""
-        return self.root.max_depth_below()
-
-    def count_nodes(self, only_leaves=False):
-        """Count the number of nodes in the tree"""
-        return self.root.count_nodes_below(only_leaves=only_leaves)
-
-    def update_bounds(self):
-        """Update the bounds of the tree"""
-        self.root.update_bounds_below()
-
-    def get_leaves(self):
-        """Get the leaves of the tree"""
-        return self.root.get_leaves_below()
-
-    def update_predict(self):
-        """Update the prediction function"""
-        self.update_bounds()
-        leaves = self.get_leaves()
-        for leaf in leaves:
-            leaf.update_indicator()
-        self.predict = lambda A: np.sum(
-            [leaf.indicator(A) * leaf.value for leaf in leaves],
-            axis=0
-        )
-
-    def np_extrema(self, arr):
-        """Return min and max of array"""
-        return np.min(arr), np.max(arr)
-
-    def random_split_criterion(self, node):
-        """Random split criterion"""
-        sub_population = self.explanatory[node.sub_population]
-        diff = 0
-        while diff == 0:
-            feature = self.rng.integers(0, self.explanatory.shape[1])
-            feature_values = sub_population[:, feature]
-            min_val, max_val = self.np_extrema(feature_values)
-            diff = max_val - min_val
-        x = self.rng.uniform()
-        threshold = (1 - x) * min_val + x * max_val
-        return feature, threshold
-
-    def get_leaf_child(self, node, sub_population):
-        """Return a leaf node"""
-        leaf_child = Leaf(value=node.depth + 1)
-        leaf_child.depth = node.depth + 1
-        leaf_child.sub_population = sub_population
-        return leaf_child
-
-    def get_node_child(self, node, sub_population):
-        """Return a regular node"""
-        n = Node()
-        n.depth = node.depth + 1
-        n.sub_population = sub_population
-        return n
-
-    def fit_node(self, node):
-        """Recursively fit nodes"""
-        node.feature, node.threshold = self.random_split_criterion(node)
-
-        left_population = node.sub_population & (
-            self.explanatory[:, node.feature] > node.threshold
-        )
-        right_population = node.sub_population & (
-            self.explanatory[:, node.feature] <= node.threshold
-        )
-
-        # Is left node a leaf?
-        is_left_leaf = (
-            node.depth + 1 >= self.max_depth or
-            np.sum(left_population) <= self.min_pop
-        )
-
-        if is_left_leaf:
-            node.left_child = self.get_leaf_child(node, left_population)
-        else:
-            node.left_child = self.get_node_child(node, left_population)
-            self.fit_node(node.left_child)
-
-        # Is right node a leaf?
-        is_right_leaf = (
-            node.depth + 1 >= self.max_depth or
-            np.sum(right_population) <= self.min_pop
-        )
-
-        if is_right_leaf:
-            node.right_child = self.get_leaf_child(node, right_population)
-        else:
-            node.right_child = self.get_node_child(node, right_population)
-            self.fit_node(node.right_child)
-
-    def fit(self, explanatory, verbose=0):
-        """Fit the isolation tree"""
+    def fit(self, explanatory, n_trees=100, verbose=0):
+        """ fits the random forest to explanatory data """
         self.explanatory = explanatory
-        self.root.sub_population = np.ones(explanatory.shape[0], dtype=bool)
-
-        self.fit_node(self.root)
-        self.update_predict()
-
+        self.numpy_preds = []
+        depths = []
+        nodes = []
+        leaves = []
+        for i in range(n_trees):
+            T = Isolation_Random_Tree(max_depth=self.max_depth,
+                                      seed=self.seed+i)
+            T.fit(explanatory)
+            self.numpy_preds.append(T.predict)
+            depths.append(T.depth())
+            nodes.append(T.count_nodes())
+            leaves.append(T.count_nodes(only_leaves=True))
         if verbose == 1:
             print(f"""  Training finished.
-    - Depth                     : {self.depth()}
-    - Number of nodes           : {self.count_nodes()}
-    - Number of leaves          : {self.count_nodes(only_leaves=True)}""")
+    - Mean depth                     : { np.array(depths).mean()}
+    - Mean number of nodes           : { np.array(nodes).mean()}
+    - Mean number of leaves          : { np.array(leaves).mean()}""")
+
+    def suspects(self, explanatory, n_suspects):
+        """ returns the n_suspects rows in explanatory
+        that have the smallest mean depth """
+        depths = self.predict(explanatory)
+        indices = np.argsort(depths)[:n_suspects]
+        return explanatory[indices], depths[indices]
