dcf725f93e1ac01bc8972f617b8eac6f26d59f7a
nsuleymanov23805 <nihad.suleymanov007@gmail.com>
2026-03-15 22:43:56 +0400

    task 3 done

---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Converts a label vector into a one-hot matrix"""
+import numpy as np
+
+
+def one_hot(labels, classes=None):
+    """returns the one-hot matrix"""
+    if classes is None:
+        classes = np.max(labels) + 1
+    one_hot_matrix = np.eye(classes)[labels]
+    return one_hot_matrix
