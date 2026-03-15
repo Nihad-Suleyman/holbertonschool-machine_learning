2b51f7899fbeb207320af1764abb0b85de3d3d9a
nsuleymanov23805 <nihad.suleymanov007@gmail.com>
2026-03-15 22:47:27 +0400

    task 4

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Trains a model using mini-batch gradient descent"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
+    """trains the model"""
+
+    history = network.fit(
+        data,
+        labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle
+    )
+
+    return history
