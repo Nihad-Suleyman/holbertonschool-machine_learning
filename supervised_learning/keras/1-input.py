8b979db71bdc8d058816448ce2242b708e77a615
nsuleymanov23805 <nihad.suleymanov007@gmail.com>
2026-03-15 22:36:22 +0400

    updated task 1

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
-import tensorflow as tf
+"""Builds a neural network using the Keras Functional API"""
+import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """builds a neural network with the Keras library"""
-
-    inputs = tf.keras.Input(shape=(nx,))
+    inputs = K.Input(shape=(nx,))
     x = inputs
-
     for i in range(len(layers)):
-        x = tf.keras.layers.Dense(
+        x = K.layers.Dense(
             units=layers[i],
             activation=activations[i],
-            kernel_regularizer=tf.keras.regularizers.l2(lambtha)
+            kernel_regularizer=K.regularizers.l2(lambtha)
         )(x)
 
         if i < len(layers) - 1:
-            x = tf.keras.layers.Dropout(rate=1 - keep_prob)(x)
+            x = K.layers.Dropout(1 - keep_prob)(x)
 
-    model = tf.keras.Model(inputs=inputs, outputs=x)
+    model = K.Model(inputs=inputs, outputs=x)
     return model
