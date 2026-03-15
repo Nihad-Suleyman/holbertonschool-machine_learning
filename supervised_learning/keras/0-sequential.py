36ba49d65e81e079a9a8a9fdd8703e401169ab06
nsuleymanov23805 <nihad.suleymanov007@gmail.com>
2026-03-15 22:24:42 +0400

    done

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,25 +1,19 @@
 #!/usr/bin/env python3
+""" 0-sequential.py """
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """builds a neural network with Keras Sequential"""
-    model = K.Sequential()
-    for i in range(len(layers)):
-        if i == 0:
-            model.add(K.layers.Dense(
-                layers[i],
-                activation=activations[i],
-                kernel_regularizer=K.regularizers.L2(lambtha),
-                input_shape=(nx,)
-            ))
-        else:
-            model.add(K.layers.Dense(
-                layers[i],
-                activation=activations[i],
-                kernel_regularizer=K.regularizers.L2(lambtha)
-            ))
-        if i < len(layers) - 1:
-            model.add(K.layers.Dropout(1 - keep_prob))
+    """ build model """
 
+    par = [K.Input(shape=(nx,))]
+    for i in range(len(layers)):
+        par.append(K.layers.Dense(
+            units=layers[i],
+            activation=activations[i],
+            kernel_regularizer=K.regularizers.l2(lambtha)
+        ))
+        if i != len(layers) - 1:
+            par.append(K.layers.Dropout(1 - keep_prob))
+    model = K.Sequential(par)
     return model
