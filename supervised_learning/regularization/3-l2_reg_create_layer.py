679eafcf7a2cfb0be223e416897b13ab9a7d1b77
nsuleymanov23805 <nihad.suleymanov007@gmail.com>
2026-03-21 21:37:58 +0400

    maybe noiw

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -7,13 +7,12 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     """
     Creates a tensorflow layer that includes L2 regularization
     """
-    regularizer = tf.keras.regularizers.l2(lambtha)
     layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
         kernel_initializer=tf.keras.initializers.VarianceScaling(
-            mode="fan_avg"
+            scale=2.0, mode="fan_avg"
         ),
-        kernel_regularizer=regularizer
+        kernel_regularizer=tf.keras.regularizers.l2(lambtha)
     )
     return layer(prev)
