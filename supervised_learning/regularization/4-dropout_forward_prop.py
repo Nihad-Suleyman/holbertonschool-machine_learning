e364303a8973f4775c93bc287480aeeb8e8180cf
nsuleymanov23805 <nihad.suleymanov007@gmail.com>
2026-03-21 21:44:01 +0400

    what about now

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -10,21 +10,18 @@ def dropout_forward_prop(X, weights, L, keep_prob):
     cache = {}
     cache["A0"] = X
     for i in range(1, L + 1):
-        W = weights["W{}".format(i)]
-        b = weights["b{}".format(i)]
-        A_prev = cache["A{}".format(i - 1)]
+        W = weights["W" + str(i)]
+        b = weights["b" + str(i)]
+        A_prev = cache["A" + str(i - 1)]
         Z = np.matmul(W, A_prev) + b
         if i == L:
-            Z_shift = Z - np.max(Z, axis=0, keepdims=True)
-            exp_Z = np.exp(Z_shift)
+            exp_Z = np.exp(Z - np.max(Z, axis=0, keepdims=True))
             A = exp_Z / np.sum(exp_Z, axis=0, keepdims=True)
-            cache["A{}".format(i)] = A
+            cache["A" + str(i)] = A
         else:
             A = np.tanh(Z)
-            D = np.random.rand(*A.shape) < keep_prob
-            A = A * D
-            A = A / keep_prob
-            cache["D{}".format(i)] = D
-            cache["A{}".format(i)] = A
-
+            D = (np.random.rand(*A.shape) < keep_prob).astype(int)
+            A = (A * D) / keep_prob
+            cache["A" + str(i)] = A
+            cache["D" + str(i)] = D
     return cache
