861ed8339dee6664c204b20c4fe0d46448dafe8b
nsuleymanov23805 <nihad.suleymanov007@gmail.com>
2026-03-21 22:40:51 +0400

    task 7

---
+++ supervised_learning/regularization/7-early_stopping.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Early stopping"""
+
+
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """
+    Determines if you should stop gradient descent early
+    """
+    if opt_cost - cost > threshold:
+        count = 0
+    else:
+        count += 1
+    if count >= patience:
+        return True, count
+
+    return False, count
