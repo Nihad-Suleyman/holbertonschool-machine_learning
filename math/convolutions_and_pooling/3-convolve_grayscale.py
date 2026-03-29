747ee9f7aa59027498c156b9ab94da679b3f33d0
nsuleymanov23805 <nihad.suleymanov007@gmail.com>
2026-03-29 22:15:49 +0400

    updated task 3

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Performs a convolution on grayscale images with stride and padding"""
+"""Performs a convolution on grayscale images"""
 import numpy as np
 
 
@@ -11,8 +11,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     kh, kw = kernel.shape
     sh, sw = stride
     if padding == 'same':
-        ph = ((h - 1) * sh + kh - h) // 2
-        pw = ((w - 1) * sw + kw - w) // 2
+        ph = int(np.ceil((((h - 1) * sh + kh - h) / 2)))
+        pw = int(np.ceil((((w - 1) * sw + kw - w) / 2)))
     elif padding == 'valid':
         ph, pw = 0, 0
     else:
@@ -22,15 +22,17 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
         ((0, 0), (ph, ph), (pw, pw)),
         mode='constant'
     )
-    output_h = (h + 2 * ph - kh) // sh + 1
-    output_w = (w + 2 * pw - kw) // sw + 1
+    output_h = ((h + 2 * ph - kh) // sh) + 1
+    output_w = ((w + 2 * pw - kw) // sw) + 1
     output = np.zeros((m, output_h, output_w))
     for i in range(output_h):
         for j in range(output_w):
             h_start = i * sh
+            h_end = h_start + kh
             w_start = j * sw
+            w_end = w_start + kw
             output[:, i, j] = np.sum(
-                padded[:, h_start:h_start + kh, w_start:w_start + kw] * kernel,
+                padded[:, h_start:h_end, w_start:w_end] * kernel,
                 axis=(1, 2)
             )
 
