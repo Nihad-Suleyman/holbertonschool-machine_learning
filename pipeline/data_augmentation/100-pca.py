8409aec348d81ddad2983084b6b222a77a9a8560
nsuleymanov23805 <nihad.suleymanov007@gmail.com>
2026-04-12 22:58:29 +0400

    maybe now

--- pipeline/data_augmentation/100-pca.py
+++ pipeline/data_augmentation/100-pca.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
 """PCA color augmentation"""
 import tensorflow as tf
-import numpy as np
 
 
 def pca_color(image, alphas):
     """Performs PCA color augmentation"""
     img = tf.cast(image, tf.float32)
-    shape = img.shape
+    shape = tf.shape(img)
     flat_img = tf.reshape(img, [-1, 3])
     mean = tf.reduce_mean(flat_img, axis=0)
     centered = flat_img - mean
     cov = tf.matmul(centered, centered, transpose_a=True) / tf.cast(tf.shape(centered)[0], tf.float32)
-    cov_np = cov.numpy()
-    eigvals, eigvecs = np.linalg.eigh(cov_np)
-    idx = np.argsort(eigvals)[::-1]
-    eigvals = eigvals[idx]
-    eigvecs = eigvecs[:, idx]
-    alphas = np.array(alphas)
-    delta = eigvecs @ (alphas * eigvals)
+    eigvals, eigvecs = tf.linalg.eigh(cov)
+    idx = tf.argsort(eigvals, direction='DESCENDING')
+    eigvals = tf.gather(eigvals, idx)
+    eigvecs = tf.gather(eigvecs, idx, axis=1)
+    alphas = tf.convert_to_tensor(alphas, dtype=tf.float32)
+    delta = tf.matmul(eigvecs, tf.expand_dims(alphas * eigvals, axis=1))
+    delta = tf.squeeze(delta)
     flat_img = flat_img + delta
     augmented = tf.reshape(flat_img, shape)
-
     return augmented
