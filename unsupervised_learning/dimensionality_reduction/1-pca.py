37394ac62d49883e6eeec67741e593cdb21a8e15
nsuleymanov23805 <nihad.suleymanov007@gmail.com>
2026-05-17 22:46:24 +0400

    task 1

---
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""PCA v2 module."""
+import numpy as np
+
+
+def pca(X, ndim):
+    """
+    Performs PCA on a dataset.
+    """
+    X_mean = np.mean(X, axis=0)
+    X_centered = X - X_mean
+
+    U, S, Vt = np.linalg.svd(X_centered)
+
+    W = Vt[:ndim].T
+
+    T = np.matmul(X_centered, W)
+
+    return T
