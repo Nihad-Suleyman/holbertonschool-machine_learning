295a6e2f48d60fcba141d6e77b5320d62d8284d5
nsuleymanov23805 <nihad.suleymanov007@gmail.com>
2026-05-17 22:44:15 +0400

    maybe now

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -9,11 +9,9 @@ def pca(X, var=0.95):
     """
     U, S, Vt = np.linalg.svd(X)
 
-    variances = S ** 2
-    total_variance = np.sum(variances)
-    cumulative_variance = np.cumsum(variances) / total_variance
+    cumulative = np.cumsum(S) / np.sum(S)
 
-    nd = np.argmax(cumulative_variance >= var) + 1
+    nd = np.argmax(cumulative >= var) + 1
 
     W = Vt[:nd].T
 
