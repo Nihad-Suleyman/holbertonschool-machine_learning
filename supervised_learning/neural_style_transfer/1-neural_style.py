d249779f8f00c775100a8acd9e757cf71bd19783
nsuleymanov23805 <nihad.suleymanov007@gmail.com>
2026-05-24 14:26:50 +0400

    task 1 done

---
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+"""Neural Style Transfer."""
+
+import numpy as np
+import tensorflow as tf
+
+
+class NST:
+    """Performs tasks for neural style transfer."""
+
+    style_layers = [
+        'block1_conv1',
+        'block2_conv1',
+        'block3_conv1',
+        'block4_conv1',
+        'block5_conv1'
+    ]
+
+    content_layer = 'block5_conv2'
+
+    def __init__(self, style_image, content_image, alpha=1e4, beta=1):
+        """Initialize NST."""
+        if (not isinstance(style_image, np.ndarray) or
+                style_image.ndim != 3 or style_image.shape[2] != 3):
+            raise TypeError(
+                'style_image must be a numpy.ndarray with shape (h, w, 3)'
+            )
+
+        if (not isinstance(content_image, np.ndarray) or
+                content_image.ndim != 3 or content_image.shape[2] != 3):
+            raise TypeError(
+                'content_image must be a numpy.ndarray with shape (h, w, 3)'
+            )
+
+        if not isinstance(alpha, (int, float)) or alpha < 0:
+            raise TypeError('alpha must be a non-negative number')
+
+        if not isinstance(beta, (int, float)) or beta < 0:
+            raise TypeError('beta must be a non-negative number')
+
+        self.style_image = self.scale_image(style_image)
+        self.content_image = self.scale_image(content_image)
+        self.alpha = alpha
+        self.beta = beta
+        self.load_model()
+
+    @staticmethod
+    def scale_image(image):
+        """Rescale an image so its largest side is 512 pixels."""
+        if (not isinstance(image, np.ndarray) or
+                image.ndim != 3 or image.shape[2] != 3):
+            raise TypeError(
+                'image must be a numpy.ndarray with shape (h, w, 3)'
+            )
+
+        h, w, _ = image.shape
+        scale = 512 / max(h, w)
+
+        new_h = int(h * scale)
+        new_w = int(w * scale)
+
+        image = image[tf.newaxis, :]
+        image = tf.image.resize(image, (new_h, new_w), method='bicubic')
+        image = image / 255
+        image = tf.clip_by_value(image, 0, 1)
+
+        return image
+
+    def load_model(self):
+        """Loads the model used to calculate cost."""
+        vgg = tf.keras.applications.VGG19(
+            include_top=False,
+            weights='imagenet'
+        )
+
+        vgg.trainable = False
+
+        outputs = [
+            vgg.get_layer(layer).output
+            for layer in self.style_layers + [self.content_layer]
+        ]
+
+        self.model = tf.keras.models.Model(
+            inputs=vgg.input,
+            outputs=outputs
+        )
