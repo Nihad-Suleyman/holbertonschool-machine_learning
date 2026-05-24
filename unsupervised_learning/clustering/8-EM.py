34dbfa6267d22ba6fc40e22b0437f9fc76490cd2
nsuleymanov23805 <nihad.suleymanov007@gmail.com>
2026-05-24 14:21:06 +0400

    8 updated

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -52,7 +52,8 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
 
         if abs(l - old_l) <= tol:
             if verbose and i % 10 != 0:
-                print("Log after {} iterations: {:.5f}".format(i, l))
+                print("Log Likelihood after {} iterations: \
+                    {:.5f}".format(i, l))
             break
 
     return pi, m, S, g, l
