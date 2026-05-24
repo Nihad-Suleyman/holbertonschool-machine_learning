6008855863a5cd55d83caa42d2e8f5d906299a3c
nsuleymanov23805 <nihad.suleymanov007@gmail.com>
2026-05-24 14:13:20 +0400

    task 10

---
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""K-means using sklearn."""
+
+import numpy as np
+import sklearn.cluster
+
+
+def kmeans(X, k):
+    """Performs K-means on a dataset."""
+    model = sklearn.cluster.KMeans(n_clusters=k)
+    model.fit(X)
+
+    C = model.cluster_centers_
+    clss = model.labels_
+
+    return C, clss
