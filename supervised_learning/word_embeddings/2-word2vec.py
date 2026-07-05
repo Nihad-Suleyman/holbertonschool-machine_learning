e1d58437182ecbaf079ccb4994ab767bba441bb0
nsuleymanov23805 <nihad.suleymanov007@gmail.com>
2026-07-05 21:23:26 +0400

    updated import for task 2

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
-Creates, builds, and trains a gensim Word2Vec model
+Train Word2Vec model
 """
 
-from gensim.models import Word2Vec
+import gensim
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
@@ -19,13 +19,13 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         negative: size of negative sampling
         cbow: True for CBOW, False for Skip-gram
         epochs: number of iterations to train over
-        seed: seed for random number generator
+        seed: seed for the random number generator
         workers: number of worker threads
 
     Returns:
         The trained Word2Vec model
     """
-    model = Word2Vec(
+    model = gensim.models.Word2Vec(
         sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
